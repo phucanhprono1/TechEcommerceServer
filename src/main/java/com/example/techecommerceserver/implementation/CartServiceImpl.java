570f8dc794e92351f2fdaf912a8c9899186889e1
Asus <phucanhprono1@gmail.com>
2023-04-24 20:11:23 +0700

    fix lỗi tính tổng hàng

--- src/main/java/com/example/techecommerceserver/implementation/CartServiceImpl.java
+++ src/main/java/com/example/techecommerceserver/implementation/CartServiceImpl.java
@@ -62,17 +62,19 @@ public class CartServiceImpl implements CartService {
 			CartItem cit = new CartItem();
 			cit.setProduct(itemOpt.get());
 			cit.setQuantity(quantity);
-			if(cart.getProduct_quantity()==null) {
-				cart.setProduct_quantity(quantity);
-			}
-			else {
-				cart.setProduct_quantity(cart.getProduct_quantity()+quantity);
-			}
+
 			cartItemRepo.save(cit);
 			cartItems.add(cit);
 		}
+		if(cart.getProduct_quantity()==null||cart.getProduct_quantity()==0) {
+			cart.setProduct_quantity(quantity);
+		}
+		else {
+			cart.setProduct_quantity(cart.getProduct_quantity()+quantity);
+		}
 		cart.setCartItems(cartItems);
 		float total = 0;
+
 		for(CartItem c:cartItems) {
 			total+=c.getProduct().getPrice()*c.getQuantity();
 		}
@@ -108,6 +110,7 @@ public class CartServiceImpl implements CartService {
 		if (!flag) {
 			throw new CartException("Product not removed from cart");
 		}
+
 		cart.setCartItems(itemList);
 		float total = 0;
 		for(CartItem c:itemList) {
@@ -129,6 +132,7 @@ public class CartServiceImpl implements CartService {
 			throw new CartException("cart not found");
 		}
 		c.getCartItems().clear();
+		c.setProduct_quantity(0);
 		float total = 0;
 		for(CartItem ca: c.getCartItems()) {
 			total+=ca.getProduct().getPrice()*ca.getQuantity();
@@ -156,8 +160,8 @@ public class CartServiceImpl implements CartService {
 		for (int i = 0; i < itemList.size(); i++) {
 			CartItem element = itemList.get(i);
 			if (element.getProduct().getProductId() == productId) {
-				cart.setProduct_quantity(cart.getProduct_quantity() + 1);
-				cart.getCartItems().get(i).setQuantity(element.getQuantity() + 1);
+//				cart.setProduct_quantity(cart.getProduct_quantity() + 1);
+				cart.getCartItems().get(i).setQuantity(cart.getCartItems().get(i).getQuantity() + 1);
 				flag = false;
 			}
 		}
@@ -165,15 +169,16 @@ public class CartServiceImpl implements CartService {
 			throw new CartException("Product not found in cart");
 		}
 		float total = 0;
+		int totalQuantity = 0;
 		for(CartItem c:itemList) {
 			total+=c.getProduct().getPrice()*c.getQuantity();
+			totalQuantity+=c.getQuantity();
 		}
 		cart.setTotal_price(total);
+		cart.setProduct_quantity(totalQuantity);
 		cRepo.save(cart);
 		return cart;
-
 	}
-
 	@Override
 	public Cart decreaseProductQuantity(Integer customerId, Integer productId)
 			throws CartException, CustomerException, ProductException {
@@ -193,7 +198,7 @@ public class CartServiceImpl implements CartService {
 			for (int i = 0; i < itemList.size(); i++) {
 				CartItem element = itemList.get(i);
 				if (element.getProduct().getProductId() == productId) {
-					cart.setProduct_quantity(cart.getProduct_quantity() - 1);
+//					cart.setProduct_quantity(cart.getProduct_quantity() - 1);
 					cart.getCartItems().get(i).setQuantity(element.getQuantity() - 1);
 					if(cart.getCartItems().get(i).getQuantity() == 0) {
 						itemList.remove(element);
@@ -207,12 +212,14 @@ public class CartServiceImpl implements CartService {
 			throw new CartException("Product not found in cart");
 		}
 		float total = 0;
+		int totalQuantity = 0;
 		for(CartItem c:itemList) {
 			total+=c.getProduct().getPrice()*c.getQuantity();
+			totalQuantity+=c.getQuantity();
 		}
 		cart.setTotal_price(total);
+		cart.setProduct_quantity(totalQuantity);
 		cRepo.save(cart);
 		return cart;
 	}
-
 }
